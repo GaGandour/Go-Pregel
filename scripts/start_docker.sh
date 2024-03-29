6c7ebbf98bdac7a92b5e36fe506bb7ddd769dfae
Gabriel Gandour <gagandour@gmail.com>
2024-03-29 20:56:12 -0300

    script

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -7,8 +7,8 @@ fi
 # TODO: BUILD DOCKER-COMPOSE FILE
 cd ..
 docker-compose -f docker-compose.yml up -d
-echo "Starting DII with $1 workers"
-docker attach dii-master
-echo "Stopping DII containers"
+echo "Starting Pregel with $1 workers"
+docker attach pregel-master
+echo "Stopping Pregel containers"
 cd scripts
 ./stop_docker.sh
