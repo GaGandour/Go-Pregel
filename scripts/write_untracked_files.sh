47d1f84aa5e91c8f949d0b8e3aad8b8e0a084d20
Gabriel Gandour <gagandour@gmail.com>
2024-04-14 18:29:10 -0300

    cp files script

--- scripts/write_untracked_files.sh
+++ scripts/write_untracked_files.sh
@@ -1,2 +1,6 @@
 cd ..
-cp model_files/user_defined* src/graph_package/
+cp model_files/user_defined_graph_methods.go src/graph_package/
+cp model_files/user_defined_graph_types.go src/graph_package/
+cp model_files/user_defined_utils_methods.go src/graph_package/
+cp model_files/user_defined_utils_types.go src/graph_package/
+cp model_files/user_defined_value_displaying.py visualization/
